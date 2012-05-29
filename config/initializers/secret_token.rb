7f811ba480087e9e25cbc50899e64b55c386898a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-29 17:43:24 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnglishReader::Application.config.secret_token = '75ed6148a7e6dbde5f981b36296a2fa682dd63d99264bee027514055fc3dcf62012e95412a512227a26e2577a8d824dec8e83277a368ab5093e794286b7673fd'
